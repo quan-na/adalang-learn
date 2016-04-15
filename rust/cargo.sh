5aca6999e5d72cf1848a8e32406aa51e71ebd384
QuanNA <naquan2112@gmail.com>
2016-04-15 17:36:50 +0700

    added module sample

---
+++ rust/cargo.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# cargo syntax for creating and executable project
+cargo new executable_example --bin
+
+# cargo syntax for creating library
+cargo new library_sample
+
+# cargo syntax doing things with projects
+cargo build
+cargo run
