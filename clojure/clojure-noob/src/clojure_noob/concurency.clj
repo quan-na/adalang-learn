6e65d1bebbeac907f26ad6eebe1bef47bd65b2fe
QuanNA <naquan2112@gmail.com>
2016-07-25 10:52:01 +0700

    backup

--- clojure/clojure-noob/src/clojure_noob/concurency.clj
+++ clojure/clojure-noob/src/clojure_noob/concurency.clj
@@ -70,3 +70,48 @@
 @fred
 ;; => {:cuddle-hunger-level 12, :percent-deteriorated 1}
 
+;; update without checking current value
+(reset! fred {:cuddle-hunger-level 0
+              :percent-deteriorated 0})
+
+;; watch
+(defn shuffle-speed
+  [zombie]
+  (* (:cuddle-hunger-level zombie)
+     (- 100 (:percent-deteriorated zombie))))
+(defn shuffle-alert
+  [key watched old-state new-state]
+  (let [sph (shuffle-speed new-state)]
+    (if (> sph 5000)
+      (do
+        (println "Run, you fool!")
+        (println "The zombie's SPH is now " sph)
+        (println "This message brought to your courtesy of " key))
+      (do
+        (println "All's well with " key)
+        (println "Cuddle hunger: " (:cuddle-hunger-level new-state))
+        (println "Percent deteriorated: " (:percent-deteriorated new-state))
+        (println "SPH: " sph)))))
+;; add watch
+(add-watch fred :fred-shuffle-alert shuffle-alert)
+(swap! fred update-in [:percent-deteriorated] + 1)
+;; => All's well with  :fred-shuffle-alert
+;; => Cuddle hunger:  22
+;; => Percent deteriorated:  3
+;; => SPH:  2134
+(swap! fred update-in [:cuddle-hunger-level] + 30)
+;; => Run, you fool!
+;; => The zombie's SPH is now 5044
+;; => This message brought to your courtesy of :fred-shuffle-alert
+
+;; atom validation
+(defn percent-deteriorated-validator
+  [{:keys [percent-deteriorated]}]
+  (and (>= percent-deteriorated 0)
+       (<= percent-deteriorated 100)))
+(def bobby
+  (atom
+   {:cuddle-hunger-level 0 :percent-deteriorated 0}
+    :validator percent-deteriorated-validator))
+(swap! bobby update-in [:percent-deteriorated] + 200)
+;; This throws "Invalid reference state"
---
+++ llvm/basic_asm.s
@@ -0,0 +1,36 @@
+;; using clang to generate llvm code
+;> clang -Os -S -emit-llvm sample.c -o sample.ll
+
+;; >>> identifiers
+%result = mul i32 %X, 8
+
+;; global result
+@result = shl i32 %X, 3
+
+;; unnamed variables
+%0 = add i32 %X, %X           ; yields i32:%0
+%1 = add i32 %0, %0           ; yields i32:%1
+%result = add i32 %1, %1
+;; <<< identifiers
+
+;; >>> modules
+; Declare the string constant as a global constant.
+@.str = private unnamed_addr constant [13 x i8] c"hello world\0A\00"
+
+; External declaration of the puts function
+declare i32 @puts(i8* nocapture) nounwind
+
+; Definition of main function
+define i32 @main() {   ; i32()*
+  ; Convert [13 x i8]* to i8  *...
+  %cast210 = getelementptr [13 x i8], [13 x i8]* @.str, i64 0, i64 0
+
+  ; Call puts function to write out the string to stdout.
+  call i32 @puts(i8* %cast210)
+  ret i32 0
+}
+
+; Named metadata
+!0 = !{i32 42, null, !"string"}
+!foo = !{!0}
+;; <<< modules
